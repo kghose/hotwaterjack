840067e00123419e49bb60cbff17e9b127ef72f6
Kaushik Ghose <kghose@users.noreply.github.com>
2022-05-27 16:20:06 -0400

    Working data client
    Use non-blocking `recv` to retrieve data.

--- data_client.py
+++ data_client.py
@@ -1,8 +1,11 @@
+"""Invoke with number of samples to retrieve.""
+import csv
+import errno
 import socket
 import sys
 
 HOST, PORT = "192.168.0.196", 3333
-samples = 10080
+samples = int(sys.argv[1])
 vars = 4
 
 # Create a socket (SOCK_STREAM means a TCP socket)
@@ -10,11 +13,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     # Connect to server and send data
     sock.connect((HOST, PORT))
     sock.sendall(bytes(f"{samples}", "utf-8"))
+    sock.setblocking(False)
+    data = []
+    while 1:
+        try:
+            packet = sock.recv(4096)
+        except socket.error as e:
+            if e.errno == errno.EWOULDBLOCK:
+                continue
+        data += list(packet)
+        if len(data) == samples * vars:
+            break
 
-    # Receive data from the server and shut down
-    # received = str(sock.recv(100), "utf-8")  # Only for text data
-
-    data = list(sock.recv(samples * vars))
-
-for n in range(samples):
-    print(data[n * vars:(n+1)* vars])
+with open("boiler-data.csv", "w") as csvfile:
+    spamwriter = csv.writer(csvfile)
+    for n in range(samples):
+        spamwriter.writerow(data[n * vars : (n + 1) * vars])
