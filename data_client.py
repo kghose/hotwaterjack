d2bcd78ff33acb445bd423193275a8c590723356
Kaushik Ghose <kaushik.ghose@gmail.com>
2022-05-26 20:42:39 -0400

    Basic client to retrieve all samples.

--- data_client.py
+++ data_client.py
@@ -2,7 +2,7 @@ import socket
 import sys
 
 HOST, PORT = "192.168.0.196", 3333
-samples = 100
+samples = 10080
 vars = 4
 
 # Create a socket (SOCK_STREAM means a TCP socket)
@@ -14,8 +14,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     # Receive data from the server and shut down
     # received = str(sock.recv(100), "utf-8")  # Only for text data
 
-    data = sock.recv(4)
-    while(data):
-        print(list(data))
-        data = sock.recv(4)
+    data = list(sock.recv(samples * vars))
 
+for n in range(samples):
+    print(data[n * vars:(n+1)* vars])
